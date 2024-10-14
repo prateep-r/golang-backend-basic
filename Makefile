300099bb111bbbf38425baa7bc0d37e7ca123616
Prateep Ruangritkul <prateep.r@arise.tech>
2024-10-14 23:57:18 +0700

    Update database

--- Makefile
+++ Makefile
@@ -1,35 +1,51 @@
-server-up:
+
+.PHONY: docker-up
+docker-up:
 	docker compose -f ./docker-compose.local.yaml up --build --wait
 
-server-down:
+.PHONY: docker-down
+docker-down:
 	docker compose -f ./docker-compose.local.yaml down > /dev/null 2>&1
 
+.PHONY: docker-restart
+docker-restart:
+	make server-down
+	make server-up
+
+.PHONY: init-test
 init-test:
 	go install go.uber.org/mock/mockgen@latest
 	go get go.uber.org/mock/mockgen/model
 
+.PHONY: test-exercise
 test-exercise:
 	go clean -testcache && go test -race ./exercise/...
 
+.PHONY: test
 test:
 	go test -race -short -coverpkg=training/app/... -coverprofile coverage.out ./...
 
-server-restart:
-	make server-down
-	make server-up
+.PHONY: database
+database:
+	go run cmd/database/main.go
 
+.PHONY: api
 api:
 	go run cmd/api/main.go
 
+.PHONY: producer
 producer:
 	go run cmd/producer/main.go
 
+.PHONY: consumer
 consumer:
 	go run cmd/consumer/main.go
 
+.PHONY: redis
 redis:
 	go run cmd/redis/main.go
 
+.PHONY: gen-mock
 gen-mock:
 	find . -name "mock.go" -type f -delete
 	mockgen -destination app/product/mock.go -package=product training/app/product Repository
---
+++ cmd/database/main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/google/uuid"
+	"time"
+	"training/database"
+	"training/persistence"
+	"training/pointer_func"
+)
+
+func main() {
+
+	ctx := context.Background()
+
+	postgresUrl := "postgres://postgres:password@localhost:5432/postgres?sslmode=disable"
+	db := database.NewPostgresDB(postgresUrl)
+
+	productId := uuid.Must(uuid.NewV7())
+
+	// **************** Insert ******************
+	product := persistence.Product{
+		ProductId:   productId,
+		ProductName: "Coca Cola",
+		Price:       15.50,
+		CreatedBy:   "ADMIN",
+		CreatedAt:   time.Now(),
+	}
+	cmd, err := db.Exec(ctx, "insert into product(product_id, product_name, price, created_by, created_at) values($1, $2, $3, $4, $5)",
+		product.ProductId,
+		product.ProductName,
+		product.Price,
+		product.CreatedBy,
+		product.CreatedAt,
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Inserted affected: ", cmd.RowsAffected())
+
+	// **************** Update ******************
+	product.ProductName = "Pepsi"
+	product.Price = 16.20
+	product.UpdatedBy = pointer_func.ToPointer("ADMIN")
+	product.UpdatedAt = pointer_func.ToPointer(time.Now())
+
+	cmd, err = db.Exec(ctx, "update product set product_name = $1, price = $2, updated_by = $3, updated_at = $4 where product_id = $5",
+		product.ProductName,
+		product.Price,
+		product.UpdatedBy,
+		product.UpdatedAt,
+		product.ProductId,
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Updated affected: ", cmd.RowsAffected())
+
+	// **************** Select ******************
+
+	var getProduct persistence.Product
+	err = db.QueryRow(ctx, "SELECT product_id, product_name, price, created_by, created_at, updated_by, updated_at FROM product WHERE product_id = $1",
+		productId,
+	).Scan(&getProduct.ProductId,
+		&getProduct.ProductName,
+		&getProduct.Price,
+		&getProduct.CreatedBy,
+		&getProduct.CreatedAt,
+		&getProduct.UpdatedBy,
+		&getProduct.UpdatedAt,
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Select %v \n", getProduct)
+
+	// **************** Delete ******************
+	cmd, err = db.Exec(ctx,
+		"delete from product where product_id = $1",
+		productId,
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Deleted affected: ", cmd.RowsAffected())
+}
